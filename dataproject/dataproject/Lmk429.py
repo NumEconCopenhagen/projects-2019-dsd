7b2187fd8ca96144346767a8199644b6a8c48986
Selman Usta <lmk429@alumni.ku.dk>
2019-03-19 17:39:57 +0100

    importeret data og første tabel med befolkning og årstal

--- dataproject/dataproject/Lmk429.py
+++ dataproject/dataproject/Lmk429.py
@@ -1,1 +1,11 @@
 
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from pandas_datareader import wb
+pop_wb = wb.download(indicator='SP.POP.TOTL', country=['SE','DK','NO'], start=1960, end=2017)
+pop_wb = pop_wb.rename(columns = {'SP.POP.TOTL':'population'})
+pop_wb = pop_wb.reset_index()
+pop_wb.head(30)
