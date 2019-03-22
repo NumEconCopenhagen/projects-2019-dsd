031896a01d8ef31d22c5c4797f9fa429e0cb8b45
Selman Usta <lmk429@alumni.ku.dk>
2019-03-22 17:49:40 +0100

    pop gdp figur

--- dataproject/dataproject/Lmk429.py
+++ dataproject/dataproject/Lmk429.py
@@ -1,15 +1,32 @@
-
+#%%
 import pandas as pd
 import numpy as np
-
 import matplotlib.pyplot as plt
+from pandas_datareader import wb
+from numpy import array
 
 #%%
-from pandas_datareader import wb
-countries = ['DK','ZA','US','GB','CN','IN','BR','CA','RU','KR','VN','SE','DE','FR','BG','IT','PK','ID','MX','PL']
-pop_wb = wb.download(indicator='SP.POP.TOTL', country=countries, start=1960, end=2017)
-pop_wb = pop_wb.rename(columns = {'SP.POP.TOTL':'population'})
-pop_wb = pop_wb.reset_index()
-pop_wb.head(174)
+countries = ['DK','ZA','TR','AL','US','GB','CN','IN','BR','CA','RU','KR','VN','SE','DE','FR','BG','IT','PK','ID','MX','PL']
+
 pop = wb.download(indicator='SP.POP.TOTL',    country=countries, start=1970, end=2017)
 gdp = wb.download(indicator='NY.GDP.PCAP.KD', country=countries, start=1970, end=2017)
+
+#pop = pop.rename(columns = {'SP.POP.TOTL':'population'})
+#pop = pop.reset_index()
+#gdp = gdp.rename(columns = {'NY.GDP.PCAP.KD' :'GDP'})
+#gdp = gdp.reset_index()
+
+GDPpop = pd.merge(gdp, pop,how='inner',on=['country', 'year'])
+GDPpop = GDPpop.rename(columns = {'NY.GDP.PCAP.KD' :'gdp', 'SP.POP.TOTL':'population'})
+GDPpop = GDPpop.reset_index()
+
+#%%
+figur1 = plt.figure()
+figur1 = plt.subplot(111)
+GDPpop.set_index('year').groupby('country')['gdp'].plot(kind='line', legend=True);
+figur1.set_ylabel('gdp');
+box = figur1.get_position() # find plot coordinates 
+figur1.set_position([box.x0, box.y0 + box.height * 0.1,box.width, box.height * 0.9]) # shrink height by 10% at bottom 
+figur1.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),ncol=5); # Put a legend below current axis
+  
+
