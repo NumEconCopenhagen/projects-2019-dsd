7582bf62f2ac788079ca52ea3c673b593519ab0f
Selman Usta <lmk429@alumni.ku.dk>
2019-03-25 15:09:55 +0100

    bedre opsætning

--- dataproject/dataproject/Lmk429.py
+++ dataproject/dataproject/Lmk429.py
@@ -6,27 +6,34 @@ from pandas_datareader import wb
 from numpy import array
 
 #%%
+#GDP constant 2010 USdollar
+
 countries = ['DK','ZA','TR','AL','US','GB','CN','IN','BR','CA','RU','KR','VN','SE','DE','FR','BG','IT','PK','ID','MX','PL']
 
-pop = wb.download(indicator='SP.POP.TOTL',    country=countries, start=1970, end=2017)
-gdp = wb.download(indicator='NY.GDP.PCAP.KD', country=countries, start=1970, end=2017)
+variables = ['SP.POP.TOTL', 'NY.GDP.MKTP.KD','NY.GDP.PCAP.KD']
 
-#pop = pop.rename(columns = {'SP.POP.TOTL':'population'})
-#pop = pop.reset_index()
-#gdp = gdp.rename(columns = {'NY.GDP.PCAP.KD' :'GDP'})
-#gdp = gdp.reset_index()
+df_names = {
+    'SP.POP.TOTL':'population',
+    'NY.GDP.MKTP.KD' : 'GDP',
+    'NY.GDP.PCAP.KD' :'GDPcap'
 
-GDPpop = pd.merge(gdp, pop,how='inner',on=['country', 'year'])
-GDPpop = GDPpop.rename(columns = {'NY.GDP.PCAP.KD' :'gdp', 'SP.POP.TOTL':'population'})
-GDPpop = GDPpop.reset_index()
+}
+
+df =  wb.download(indicator=variables, country=countries, start=1970, end=2017).reset_index()
+df = df.rename(columns=df_names)
+df.head()
+
+#df[""] = df[""] / df[""]
+
 
 #%%
 figur1 = plt.figure()
 figur1 = plt.subplot(111)
-GDPpop.set_index('year').groupby('country')['gdp'].plot(kind='line', legend=True);
-figur1.set_ylabel('gdp');
+df.set_index('year').groupby('country')[["year", 'GDPcap']].plot(x= "year", y="GDPcap", kind='line', legend=True)
+figur1.set_ylabel('GDPcap')
+figur1.set_ylabel('GDPcap')
 box = figur1.get_position() # find plot coordinates 
 figur1.set_position([box.x0, box.y0 + box.height * 0.1,box.width, box.height * 0.9]) # shrink height by 10% at bottom 
-figur1.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),ncol=5); # Put a legend below current axis
+figur1.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),ncol=5) # Put a legend below current axis
   
 
