ee2948dc654b50f5e5a41852ac4229a729fc101c
Selman Usta <lmk429@alumni.ku.dk>
2019-03-25 15:21:28 +0100

    ny

--- dataproject/dataproject/Lmk429.py
+++ dataproject/dataproject/Lmk429.py
@@ -9,9 +9,7 @@ from numpy import array
 #GDP constant 2010 USdollar
 
 countries = ['DK','ZA','TR','AL','US','GB','CN','IN','BR','CA','RU','KR','VN','SE','DE','FR','BG','IT','PK','ID','MX','PL']
-
 variables = ['SP.POP.TOTL', 'NY.GDP.MKTP.KD','NY.GDP.PCAP.KD']
-
 df_names = {
     'SP.POP.TOTL':'population',
     'NY.GDP.MKTP.KD' : 'GDP',
@@ -25,15 +23,14 @@ df.head()
 
 #df[""] = df[""] / df[""]
 
-
 #%%
 figur1 = plt.figure()
 figur1 = plt.subplot(111)
-df.set_index('year').groupby('country')[["year", 'GDPcap']].plot(x= "year", y="GDPcap", kind='line', legend=True)
-figur1.set_ylabel('GDPcap')
+df.set_index('year').groupby('country')['GDPcap'].plot(kind='line', legend=True)
 figur1.set_ylabel('GDPcap')
 box = figur1.get_position() # find plot coordinates 
 figur1.set_position([box.x0, box.y0 + box.height * 0.1,box.width, box.height * 0.9]) # shrink height by 10% at bottom 
 figur1.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),ncol=5) # Put a legend below current axis
   
-
+#merged.set_index('year').groupby('country')['pop'].plot(kind='line',legend=True);
+#fig1.set_ylabel('GDP');
