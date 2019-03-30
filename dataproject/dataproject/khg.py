7cf4e78e4bb661d118dba543cd882d44de79c1e5
unknown <khg783@alumni.ku.dk>
2019-03-30 22:15:29 +0100

    hi

--- dataproject/dataproject/khg.py
+++ dataproject/dataproject/khg.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import ipywidgets as widgets
 
 #countries = ['DK','ZA','US','GB','CN','IN','BR','CA','RU','TR','KR','VN','SE','DE','AL','FR','BG','IT','PK','ID','MX','PL']
 
@@ -44,15 +45,44 @@ def plot(fig):
 
 plot(merged)
 
-# Generating first and last observation:
+# Yearly growth rate in GDP and population:
+
+merged_yearly = merged.groupby('country')['gdp_cap'].pct_change()
+merged_yearly.head(50)
+
+# Growth in the period 1970-2017 for each continent:
+
+merged_grouped_last = merged_grouped.gdp_cap.last()
+merged_grouped_last.name = 'last'
+
+merged.set_index(['country','year'],inplace=True)
+merged = merged.join(merged_grouped_last)
+merged.reset_index(inplace=True)
+
+merged['g_total'] = merged['last']/merged['first']*100
+
+def plot(dataframe, country):
+    I = dataframe['country'] == country
+    ax = dataframe.loc[I,:].plot(x='year', y = 'pop', style = '-o', legend = 'False')
+
+widgets.interact(plot, 
+    dataframe = widgets.fixed(merged),
+    country = widgets.Dropdown(description='country', options=merged.country.unique(), value='Europe & Central Asia')
+);
+
+
+
+
+
+
+# Generating first and last observation and growth rate over the 
+# whole period:
 
 first = merged.groupby('country')['gdp_cap','pop'].head(1).reset_index()
 last = merged.groupby('country')['gdp_cap','pop'].tail(1).reset_index()
 
 print(first)
 print(last)
-
-# Generating the growth rate 1970-2017 for GDP per capita and pop:
 
 growth = first/last*100
 print(growth)
@@ -77,8 +107,7 @@ ax.set_ylabel('Avg. one-year growth in population');
 fig1 = plt.figure()
 fig1 = plt.subplot(111)
 merged.set_index('year').groupby('country')['pop'].plot(kind='line',legend=True);
-fig1.set_ylabel('GDP');
-fig1.invert_xaxis();
+fig1.set_ylabel('Pop');
 box = fig1.get_position()
 fig1.set_position([box.x0, box.y0 + box.height * 0.1,box.width, box.height * 0.9])
 fig1.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),ncol=5);
@@ -88,7 +117,6 @@ fig2 = plt.figure()
 fig2 = plt.subplot(111)
 merged.set_index('year').groupby('country')['gdp'].plot(kind='line',legend=True);
 fig2.set_ylabel('GDP');
-fig2.invert_xaxis();
 box = fig2.get_position()
 fig2.set_position([box.x0, box.y0 + box.height * 0.1,box.width, box.height * 0.9])
 fig2.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),ncol=5);
